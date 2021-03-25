6d186a608ae4c2cfcc4919a1df5e51776ffd3259
Sneharsh Belsare <snehbelsare@gmail.com>
2021-03-25 14:56:57 +0530

    Added drawer functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hapn_app/views/homepage.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -6,7 +7,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData.dark(),
     );
   }
 }
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:hapn_app/views/my_drawer.dart';
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-            )
-          ],
+        centerTitle: true,
+        title: Text.rich(
+          TextSpan(
+            text: "HAP",
+            style: TextStyle(
+              fontSize: 22
+            ),
+            children: [
+              TextSpan(
+                text: "N",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.blue
+                )
+              )
+            ]
+          )
         ),
         backgroundColor: Colors.transparent,
         elevation: 0.0,
       ),
+      drawer: MyDrawer(),
     );
   }
 }
